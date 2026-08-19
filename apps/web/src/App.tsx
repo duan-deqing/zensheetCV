c79e6d267f222fb33f79bd8c1ea87649315c9d21
duan-deqing <2030851914@qq.com>
2026-08-19 20:35:44 +0800

    feat(editor): integrate sidebar, topbar, and toast into editor layout

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,23 +2,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { EditorProvider } from '@/store/EditorContext';
 import { ResumeProvider } from '@/store/ResumeContext';
 import { PreviewProvider } from '@/store/PreviewContext';
+import { UIProvider } from '@/store/UIContext';
 import { EditorPage } from '@/pages/EditorPage';
 import { HomePage } from '@/pages/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
-      <EditorProvider>
-        <ResumeProvider>
-          <PreviewProvider>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/editor" element={<EditorPage />} />
-              <Route path="/editor/:id" element={<EditorPage />} />
-            </Routes>
-          </PreviewProvider>
-        </ResumeProvider>
-      </EditorProvider>
+      <UIProvider>
+        <EditorProvider>
+          <ResumeProvider>
+            <PreviewProvider>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/editor" element={<EditorPage />} />
+                <Route path="/editor/:id" element={<EditorPage />} />
+              </Routes>
+            </PreviewProvider>
+          </ResumeProvider>
+        </EditorProvider>
+      </UIProvider>
     </BrowserRouter>
   );
 }
--- apps/web/src/pages/EditorPage.tsx
+++ apps/web/src/pages/EditorPage.tsx
@@ -1,18 +1,30 @@
 import { MarkdownEditor } from '@/editor/MarkdownEditor';
 import { ResumePreview } from '@/preview/ResumePreview';
+import { ResumeList } from '@/components/ResumeList';
+import { TopBar } from '@/components/TopBar';
+import { Toast } from '@/components/Toast';
 import { useAutoSave } from '@/hooks/useAutoSave';
+import { useKeyboardShortcut } from '@/hooks/useKeyboardShortcut';
 
 export function EditorPage() {
   useAutoSave();
+  useKeyboardShortcut();
 
   return (
-    <div className="flex h-screen bg-gray-50">
-      <div className="w-1/2 p-3">
-        <MarkdownEditor />
+    <div className="flex flex-col h-screen bg-gray-50">
+      <TopBar />
+      <div className="flex flex-1 overflow-hidden">
+        <ResumeList />
+        <div className="flex flex-1">
+          <div className="w-1/2 p-3">
+            <MarkdownEditor />
+          </div>
+          <div className="w-1/2 p-3">
+            <ResumePreview />
+          </div>
+        </div>
       </div>
-      <div className="w-1/2 p-3">
-        <ResumePreview />
-      </div>
+      <Toast />
     </div>
   );
 }
